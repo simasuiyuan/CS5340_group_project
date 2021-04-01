cd9998ad192a30e850c8424458a35104f788e252
dehui <39208457+dehui333@users.noreply.github.com>
2021-04-01 22:44:48 +0800

    Update BN_Inference.py
    update BN_Inference to accomodate pgmpy/bnlearn models and MAP output or output the posterior distribution

--- source/BN_Inference.py
+++ source/BN_Inference.py
@@ -1,5 +1,7 @@
 from pgmpy.inference import VariableElimination
 import pandas as pd
+import bnlearn
+import numpy as np
 '''
 Inputs:
     1. model: 
@@ -14,18 +16,51 @@ Inputs:
     3. to_predict
     The list of variables to predict as a list of strings.
     
+    4. model_type
+    'pgypy' or 'bnlearn'
+    
+    5. output_type
+    'dist' or 'map'
+    'dist' to return the posterior distribution
+    'map' to return a list of dictionaries mapping the variables to the MAP values.
+    
+    
 Output:
     predictions:
-    A list of dictionaries. Mapping the predicted variables to the predicted values for each row of the
-    input evidence data.
+        Depends on output_type:
+        'dist' or 'map'
+        'dist' to return the posterior distribution
+        'map' to return a list of dictionaries mapping the variables to the MAP values.
 
 '''
-def infer_with_model(model, evidence_data, to_predict):
+def infer_with_model(model, evidence_data, to_predict, model_type='pgmpy', output_type='map'):
     predictions = []
-    infer = VariableElimination(model)
     evidences = evidence_data.to_dict(orient='records')
-    for evidence in evidences:
-        predictions.append(infer.map_query(to_predict, evidence))
+    if model_type=='pgmpy':
+        infer = VariableElimination(model)
+        for evidence in evidences:
+            if output_type=='map':
+                predictions.append(infer.map_query(to_predict, evidence))
+            elif output_type=='dist':
+                predictions.append(infer.query(to_predict, evidence))
+            else:
+                print('Unknown model type')
+    elif model_type=='bnlearn':
+        for evidence in evidences:
+            if output_type=='map':
+                prediction = bnlearn.inference.fit(model, to_predict, evidence)
+                values = prediction.values
+                variables = prediction.variables
+                indices = np.unravel_index(values.argmax(), values.shape)
+                d = dict()
+                for i in range(len(variables)):
+                    variable = variables[i]
+                    d[variable] = indices[i]
+                predictions.append(d)
+            elif output_type=='dist':
+                predictions.append(bnlearn.inference.fit(model, to_predict, evidence))
+            else:
+                print('Unknown model type')
     return predictions
     
 '''
@@ -39,5 +74,5 @@ Output:
     
 '''
 
-def categorical_prediction_to_projections(predictions):
+def categorical_prediction_to_projection(predictions):
     return projections
